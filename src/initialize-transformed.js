ef4a7206e66e2c0642a9d034ccce63daa45ea503
Shiben Dutta <fit@qlik.com>
2019-10-23 18:03:59 +0530

    handling undefined in sourceRow[index]

--- src/initialize-transformed.js
+++ src/initialize-transformed.js
@@ -167,23 +167,33 @@ function appendMissingCells (
 
   let index = sourceIndex;
   measurements.forEach((measurement, measureIndex) => {
-    if (index < sourceRow.length) {
+    if (index < sourceRow.length
+      && (dim2ElementNumber === -1
+        || sourceRow[index].parents.dimension2.elementNumber === dim2ElementNumber)
+      && sourceRow[index].parents.measurement.header === measurement.name) {
       // Source contains the expected cell
       destRow.push(sourceRow[index]);
       index++;
     } else {
       // Source doesn't contain the expected cell, so add empty
-      destRow.push({
-        displayValue: '',
-        parents: {
-          dimension1: { elementNumber: dim1ElementNumber },
-          dimension2: { elementNumber: dim2ElementNumber },
-          measurement: {
-            header: measurement.name,
-            index: measureIndex
+      /*Bug fix QLIK-97295 */
+      if (typeof sourceRow[index] === 'undefined') { //sourceRow[index] gets undefined values when index is out of range in sourceRow Array
+        destRow.push({
+          displayValue: '',
+          parents: {
+            dimension1: { elementNumber: dim1ElementNumber },
+            dimension2: { elementNumber: dim2ElementNumber },
+            measurement: {
+              header: measurement.name,
+              index: measureIndex
+            }
           }
-        }
-      });
+        });
+        index++;
+      } else {
+        destRow.push(sourceRow[index]);
+        index++;
+      }/*Bug fix QLIK-97295 */
     }
   });
   return index;
